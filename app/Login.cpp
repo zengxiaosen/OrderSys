227f4d36550f44d731d1189a0b5c8339f878e07a
zengxiaosen <919451814@qq.com>
2018-04-13 12:18:15 +0800

    test

---
+++ app/Login.cpp
@@ -0,0 +1,150 @@
+#include "OBOJni.h"
+#include "curl/curl.h"
+#include "cJSON.h"
+#include "Data.h"
+typedef struct login_response_data{
+    login_response_data(){
+        memset(data, 0, RESPONSE_DATA_LEN);
+        data_len = 0;
+    }
+    char data[RESPONSE_DATA_LEN];
+    int data_len;
+}login_response_data_t;
+
+size_t curl_login_callback(char* ptr, size_t n, size_t m, void* userdata){
+    login_response_data_t *data = (login_response_data_t*)userdata;
+    int response_data_len = n*m;
+    memcpy(data->data +  data->data_len, ptr, response_data_len);
+    data->data_len += response_data_len;
+    return response_data_len;
+}
+
+extern "C"{
+    JNIEXPORT jboolean JNICALL Java_com_example_ace_obo_OBOJni_Login(JNIEnv *env, jobject obj, jstring jusername, jstring jpasswd, jboolean jisDriver){
+        const char* username = env->GetStringUTFChars(jusername, NULL);
+        const char* password = env->GetStringUTFChars(jpasswd, NULL);
+        const char* isDriver = (jisDriver==JNI_TRUE)?"true":"no";
+        JNIINFO("LOGIN: username = %s, passwd = %s, isDriver = %s", username, passwd, isDriver);
+        //保存当前角色
+        Data::getInstance()->setIsDriver(isDriver);
+        //设置当前orderid为空
+        Data::getInstance()->setOrderid("NONE");
+        //更改当前角色状态
+        if(jisDriver == JNI_TRUE){
+            DATA::getInstance()->setStatus(OBO_STATUS_DRIVER_IDLE);
+        }else{
+            DATA::getInstance()->setStatus(OBO_STATUS_PASSNGER_IDLE);
+        }
+        /**
+         * 给服务器段的协议 https://ip:port/login [json_data]
+         * {
+         *      username: "aaa",
+         *      passwd: "bbb",
+         *      driver: "yes"
+         * }
+         * 
+         * 得到服务端响应数据
+         * //成功
+         * {
+         *      result: "ok",
+         *      sessionid: "xxxxxxx"
+         * }
+         * 
+         * //失败
+         * {
+         *      result: "error",
+         *      reason: "why...."
+         * }
+         * 
+         * */
+        //封装登录json字符串
+        cJSON *root = CJSON_CreateObject();
+        cJSON_AddStringToObject(root, "username", username);
+        cJSON_AddStringToObject(root, "password", passwd);
+        cJSON_AddStringToObject(root, "driver", isDriver);
+
+        char* json_str = cJSON_Print(root);
+        cJSON_Delete(root);
+
+
+        //发送curl请求：
+        CURLcode curl_ret;
+        CURL* curl = curl_easy_init();
+
+        login_response_data_t res_data;
+        const char *uri = "https://ip:port/login";
+
+        curl_easy_setopt(curl, CURLOPT_URL, uri);
+        curl_easy_setopt(curl, CURLOPT_POST, true);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_str);
+
+        /**
+         * 如果用HTTPS协议，额外需要添加的部分
+         * */
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_login_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &res_data);
+
+        curl_ret = curl_easy_perform(curl);
+        if(curl_ret != CURL_OK){
+            JNIINFO("curl perform error %s\n%s", uri, json_str);
+            return JNI_FALSE;
+        }
+        curl_easy_cleanup(curl);
+        free(json_str);
+
+        /**
+         * 得到服务器响应数据
+         * //成功
+         * {
+         *      result: "ok",
+         *      recode: "0",
+         *      sessionid: "xxxxxx"
+         * }
+         * //失败
+         * {
+         *      result: "error",
+         *      reason: "why...."
+         * }
+         * 
+         * */
+
+        res_data.data[res_data.data_len] = '\0';
+        bool login_succ = false;
+        
+        root = cJSON_Parse(res_data.data);
+        cJSON* result = cJSON_GetObjectItem(root, "result");
+        if(result && (strcmp(result->valuestring, "ok") == 0)){
+            //succ
+            Data::getInstance()->setSessionid(cJSON_GetObjectItem(root, "sessionid")->valuestring);
+            JNIINFO("login succ, sessionid=%s", Data::getInstance()->getSessionid().c_str());
+            login_succ = true;
+        }else{
+            //fail
+            cJSON *reason = cJSON_GetObjectItem(root, "reason");
+            if(reason){
+                JNIINFO("Login error, reason = %s", reason->valuestring);
+            }
+            else{
+                JNIINFO("Login error, unknow reason, res_data=%s", res_data.data);
+            }
+            login_succ = false;
+        }
+        CJSON_Delete(root);
+
+
+        //释放字符串资源
+        env->ReleaseStringUTFChars(jusername, username);
+        env->ReleaseStringUTFChars(jpasswd, passwd);
+
+        if(login_succ = true){
+            return JNI_TRUE;
+        }
+        else{
+            return JNI_FALSE;
+        }
+    }
+}
+
---
+++ app/OBOJni.h
@@ -0,0 +1,27 @@
+
+
+#ifndef OBO_OBOJNI_H
+#define OBO_OBOJNI_H
+
+
+#include <stdio.h>
+#include <string>
+#include <jni.h>
+#include <android/log.h>
+
+using namespace std;
+
+#define jniLogTag           "JNI"
+#define OBO_SERVER_IP       ""
+#define OBO_SERVER_PORT     "18888"
+
+#define RESPONSE_DATA_LEN   (4096)
+#define TIME_STR_LEN        (64)
+
+#define JNIINFO(fmt, ...) \
+    __android_log_print(ANDROID_LOG_INFO, jniLogTag, fmt, __VA_ARGS__);
+
+
+
+
+#endif //OBO_OBOJNI_H
